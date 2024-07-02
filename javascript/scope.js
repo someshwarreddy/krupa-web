ad0685a2322a16558f30029dec89f2581207369e
someshwar reddy <somesh.satish@gmail.com>
2024-07-02 16:18:23 +0530

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,42 @@
+// global scope 
+
+var varglobalValue= 'i am global';
+let letglobalValue = ' i am let global'
+console.log(letglobalValue);
+
+// function scope
+// var always function scoped 
+function named() {
+    var varfumctionValue= 'i am var functionvalue';
+    var varglobalValue= 'i am function re assgined value ';
+    console.log(varglobalValue);
+    console.log(varfumctionValue);  
+};
+
+var unnamed = function() {
+    var varfumctionValue = 'i am var functionvalue';
+    let varglobalValue= 'i am function re assgined value ';
+    console.log(varfumctionValue); 
+    console.log(varglobalValue); 
+}
+console.log(varfumctionValue);
+console.log(varglobalValue);
+named();
+unnamed();
+console.log(varglobalValue);
+console.log(varglobalValue);
+console.log(letglobalValue);
+
+// blockscope
+
+// let block scoped
+
+{
+    let letglobalValue= 'i am let blockvalue';
+    var blockscope = " i am var avialible to outside of block scope"
+    console.log(letglobalValue);  
+}
+
+console.log("letblockvalue "+ blockscope); 
+
+console.log(letglobalValue);
